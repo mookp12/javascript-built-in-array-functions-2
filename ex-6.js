a978c320e7f6d628c6b6ee8872b7c092d9a18840
Anuna <anuaq20@gmail.com>
2025-07-29 13:53:37 +0700

    feat: implement palindrome check with string trimming and reversal

--- ex-6.js
+++ ex-6.js
@@ -3,7 +3,7 @@ const carCollection = ["toyota", "fiat", "honda", "bmw"];
 function updateCarCollection(carBrand) {
   // Start coding here
   if (carCollection.includes(carBrand)) {
-    let position = carCollection.indexOf(carBrand); // index ของ brand ที่ซ้ำ
+    let position = carCollection.indexOf(carBrand); 
     return `${carBrand} already exists in position ${position} of the car collection.`;
   } else {
     carCollection.push(carBrand);
